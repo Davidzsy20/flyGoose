e1cb9d60609d7de08a54651b4259529243b47e24
David Zou <davidzsy20@gmail.com>
2017-11-23 02:39:09 -0500

    day1 style nav

--- flygoose/src/Bar.js
+++ flygoose/src/Bar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
- 
+import './Bar.css'
 const Bar = () => (
-  <RaisedButton label="Default" classname= 'raisebutton' />
+  <RaisedButton label="Default" className='b' />
 );
  
 export default Bar;
--- flygoose/src/Nav.js
+++ flygoose/src/Nav.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
  import './Nav.css';
  import Bar from './Bar';
+ import 'materialize-css';
+ import 'materialize-css/dist/css/materialize.min.css';
 class Nav extends Component {
     render(){
         return (
             <nav className='Nav'>
-                <Bar/>
+                <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Logo</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="sass.html">Students</a></li>
+        <li><a href="badges.html">Class</a></li>
+        <li><a href="collapsible.html">About</a></li>
+        </ul>
+                {/* <Bar/>  */}
+                </div>
             </nav>
         ) 
     }
